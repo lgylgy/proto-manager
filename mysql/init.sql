5b4aef00c087e94a497400fa09cc3bd79491853e
Loic Geffroy <loic.geffroy@masagroup.net>
2021-03-28 16:30:08 +0100

    rename column names

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `projects` (
-	`ProjectID` INT NOT NULL,
-	`ProjectName` TEXT NOT NULL,
-	`Description` TEXT,
-	PRIMARY KEY (`ProjectID`)
+	`project_id` INT NOT NULL,
+	`project_name` TEXT NOT NULL,
+	`description` TEXT,
+	PRIMARY KEY (`project_id`)
 );
 
-INSERT INTO projects (ProjectID, ProjectName, Description) VALUES (1, 'Rinkoid', 'description');
-INSERT INTO projects (ProjectID, ProjectName, Description) VALUES (2, 'MPGScore', '');
+INSERT INTO projects (project_id, project_name, description) VALUES (1, 'Rinkoid', 'description');
+INSERT INTO projects (project_id, project_name, description) VALUES (2, 'MPGScore', '');
